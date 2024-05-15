dd4182f43370f363539fa1a8da3e5787f595f2ad
thiger7 <adf0423@gmail.com>
2024-05-16 08:23:56 +0900

    ページ共通部分のLayoutコンポーネントを作成

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import App from "next/app";
 import Head from "next/head";
+import Layout from "../components/Layout";
 
 export default class MyApp extends App {
   render() {
@@ -13,7 +14,9 @@ export default class MyApp extends App {
             href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
           />
         </Head>
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </>
     );
   }
