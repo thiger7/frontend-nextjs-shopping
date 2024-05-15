622f049b9d6a9c123885469e0b54d33388b88bf0
thiger7 <adf0423@gmail.com>
2024-05-16 08:06:15 +0900

    CSSライブラリのreactstrapを install

--- pages/_app.js
+++ pages/_app.js
@@ -7,10 +7,14 @@ export default class MyApp extends App {
     const { Component, pageProps } = this.props;
     return (
       <>
-        <Head></Head>
-        <Component>{...pageProps}</Component>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+          />
+        </Head>
+        <Component {...pageProps} />
       </>
-
-    )
+    );
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import { Alert, Button } from "reactstrap";
+
 const index = () => {
-  return <div>Hello Nextjs</div>;
+  return (
+    <div>
+      <div>
+        <Alert color="primary">Hello Project</Alert>
+        <Button color="primary">Hello from nextjs</Button>
+      </div>
+    </div>
+  );
 };
 
 export default index;
